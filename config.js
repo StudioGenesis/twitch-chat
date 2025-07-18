0b111685595e3bb57078dc2cb5eb653657d65480
Genesis <genesis@studiogenesis.com.au>
2025-07-18 11:11:19 +1000

    Refactor chat configuration to use dynamic values for channel, emote set ID, and badge URLs

---
+++ config.js
@@ -0,0 +1,11 @@
+window.chatConfig = {
+  channel: "studiogenesisau",
+  seventvEmoteSetId: "01H4X8GZFR0004TC809Z51977V",
+  badgeUrls: {
+    moderator: "badges/moderator.png",
+    broadcaster: "badges/broadcaster.png",
+    vip: "badges/vip.png",
+    // Add more as needed
+    //subscriber: "badges/subscriber.png"
+  }
+};
